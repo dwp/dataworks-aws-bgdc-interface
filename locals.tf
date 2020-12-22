5384a174a61274dbcb0de2f03018b486579c947a
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2020-12-22 12:12:20 +0000

    Add security groups

---
+++ locals.tf
@@ -0,0 +1,21 @@
+locals {
+
+  crypto_workspace = {
+    management-dev = "management-dev"
+    management     = "management"
+  }
+
+  management_workspace = {
+    management-dev = "default"
+    management     = "management"
+  }
+
+  management_account = {
+    development = "management-dev"
+    qa          = "management-dev"
+    integration = "management-dev"
+    preprod     = "management"
+    production  = "management"
+  }
+
+}
