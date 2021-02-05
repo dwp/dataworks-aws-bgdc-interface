c31ff8787bbccccfed5c4d5d5ed86d43f52be7ca
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2021-02-05 13:16:50 +0000

    Fix asset release download

--- get_lambda_release.sh
+++ get_lambda_release.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+REPO=$1
 VERSION=$(curl --silent "https://api.github.com/repos/dwp/${REPO}/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 echo "my version is $VERSION"
 echo "$(echo ${REPO} | sed 's/-/_/g')_zip = { base_path = \"../${REPO}-release\", version = \"${VERSION}\" }" >> terraform.tfvars
 mkdir ../${REPO}-release
 ASSET=$(curl --silent "https://api.github.com/repos/dwp/${REPO}/releases/latest" | grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \")
-(cd ../${REPO}-release/ && curl -O ${ASSET})
+cd ../${REPO}-release/ && curl -L -O ${ASSET}
