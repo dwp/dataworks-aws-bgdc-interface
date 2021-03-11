fdb491a28d1e50e4dc47514f327c1cb00043a201
Dmitrijs Kovalenko <dmitrjiskovalenko@digital.uc.dwp.gov.uk>
2021-03-11 19:11:30 +0000

    Drop unneeded R53 record

--- profiling_node_vpc_endpoints.tf
+++ profiling_node_vpc_endpoints.tf
@@ -15,25 +15,12 @@ resource "aws_vpc_endpoint" "bgdc_interface_vpce" {
   vpc_id              = module.edc_vpc.vpc.id
   service_name        = aws_vpc_endpoint_service.bgdc_interface_vpce.service_name
   vpc_endpoint_type   = "Interface"
-  subnet_ids          = [aws_subnet.edc.*.id]
+  subnet_ids          = aws_subnet.edc.*.id
   private_dns_enabled = false
 
   security_group_ids = [
     aws_security_group.bgdc_interface_vpce.id,
   ]
-}
-
-resource "aws_route53_record" "bgdc_interface_vpce" {
-  provider = aws.management_mgmt
-  zone_id  = data.terraform_remote_state.management_mgmt.outputs.dataworks_zone.id
-  name     = "bgdc_interface_vpce.${local.env_prefix[local.environment]}"
-  type     = "A"
-
-  alias {
-    name                   = aws_vpc_endpoint.bgdc_interface_vpce.dns_entry.dns_name
-    zone_id                = aws_vpc_endpoint.bgdc_interface_vpce.dns_entry.hosted_zone_id
-    evaluate_target_health = false
-  }
 }
 
 resource "aws_security_group" "bgdc_interface_vpce" {
