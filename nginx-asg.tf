1406fc8c0d18bea415d2631c9deb3c55314acba4
ali.fener <ali.fener@engineering.digital.dwp.gov.uk>
2021-10-22 14:48:19 +0100

    lifecycle added for loadbalancer

--- nginx-asg.tf
+++ nginx-asg.tf
@@ -67,6 +67,10 @@ resource "aws_lb" "dwx_bdgc_nginx_nlb" {
 
   enable_deletion_protection = false
 
+  lifecycle {
+    create_before_destroy = true
+  }
+
   tags = {
     Environment = local.environment
   }
@@ -140,7 +144,7 @@ resource "aws_lb_listener" "dwx_bdgc_nginx_emr_nlb_listener" {
 resource "aws_vpc_endpoint_service" "bgdc_dwx_end_point_service" {
   acceptance_required        = false
   network_load_balancer_arns = [aws_lb.dwx_bdgc_nginx_nlb.arn]
-  allowed_principals = ["arn:aws:iam::${local.account[local.environment]}:root"]
+  allowed_principals         = ["arn:aws:iam::${local.account[local.environment]}:root"]
   tags = {
     Name = "bgdc-dwx-endpoint-svc"
   }
