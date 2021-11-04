f7e3c90dbd7046e2e0cadf55376d0355bfabad3d
ali.fener <ali.fener@engineering.digital.dwp.gov.uk>
2021-11-04 12:08:36 +0000

    step updates

--- steps.tf
+++ steps.tf
@@ -29,7 +29,7 @@ resource "aws_s3_bucket_object" "ghostunnel-setup" {
 resource "aws_s3_bucket_object" "emr-nlb-attachment" {
   //for_each = local.emr_clusters
   bucket   = data.terraform_remote_state.common.outputs.config_bucket.id
-  key      = "component/${local.component[each.key]}/emr-nlb-attachment.sh"
+  key      = "component/${local.component}/emr-nlb-attachment.sh"
   content = templatefile("${path.module}/steps/emr-nlb-attachment.tpl",
     {
       aws_default_region             = "eu-west-2"
